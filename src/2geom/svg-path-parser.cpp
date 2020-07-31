17e0d21f0afc8489656f9184bff7ad024a42394a
Thomas Holder <thomas@thomas-holder.de>
2020-07-31 21:52:44 +0200

    Fix incorrect vim modeline

--- src/2geom/svg-path-parser.cpp
+++ src/2geom/svg-path-parser.cpp
@@ -1612,4 +1612,4 @@ void parse_svg_path_file(FILE *fi, PathSink &sink)
   fill-column:99
   End:
 */
-// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=99 :
+// vim: filetype=ragel:cindent:expandtab:shiftwidth=4:softtabstop=4:fileencoding=utf-8:textwidth=99 :
