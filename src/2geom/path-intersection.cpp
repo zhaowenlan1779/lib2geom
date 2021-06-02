ce387c0e60d6488a3ff4e2bddb715e580d874864
Thomas Holder <thomas@thomas-holder.de>
2021-06-02 20:51:17 +0200

    Don't use `bool pdx = 2` in `path_mono_splits`
    Closes https://gitlab.com/inkscape/lib2geom/-/issues/28

--- src/2geom/path-intersection.cpp
+++ src/2geom/path-intersection.cpp
@@ -411,13 +411,11 @@ std::vector<double> path_mono_splits(Path const &p) {
     std::vector<double> ret;
     if(p.empty()) return ret;
     
-    bool pdx=2, pdy=2;  //Previous derivative direction
+    int pdx = 2, pdy = 2; // Previous derivative direction
     for(unsigned i = 0; i < p.size(); i++) {
         std::vector<double> spl = offset_doubles(curve_mono_splits(p[i]), i);
-        bool dx = p[i].initialPoint()[X] > (spl.empty()? p[i].finalPoint()[X] :
-                                                         p.valueAt(spl.front(), X));
-        bool dy = p[i].initialPoint()[Y] > (spl.empty()? p[i].finalPoint()[Y] :
-                                                         p.valueAt(spl.front(), Y));
+        int dx = p[i].initialPoint()[X] > (spl.empty() ? p[i].finalPoint()[X] : p.valueAt(spl.front(), X)) ? 1 : 0;
+        int dy = p[i].initialPoint()[Y] > (spl.empty() ? p[i].finalPoint()[Y] : p.valueAt(spl.front(), Y)) ? 1 : 0;
         //The direction changed, include the split time
         if(dx != pdx || dy != pdy) {
             ret.push_back(i);
