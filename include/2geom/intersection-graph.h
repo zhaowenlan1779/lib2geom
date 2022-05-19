3a4f2eac1288e91c1b2888004aff6611527779f6
Rafael Siejakowski <rs@rs-math.net>
2022-05-19 18:28:37 -0300

    Document PathIntersectionGraph
    This commit adds a more detailed documentation of the class
    `PathIntersectionGraph` which is responsible for performing
    boolean operations. There is only one code change: a "dead"
    store is removed.

--- include/2geom/intersection-graph.h
+++ include/2geom/intersection-graph.h
@@ -43,26 +43,114 @@
 
 namespace Geom {
 
+/** @class PathIntersectionGraph
+ * @brief Intermediate data for computing Boolean operations on paths.
+ *
+ * This class implements the Greiner-Hormann clipping algorithm,
+ * with improvements inspired by Foster and Overfelt as well as some
+ * original contributions.
+ *
+ * For the purposes of boolean operations, a shape is defined as a PathVector
+ * using the "even-odd" rule, i.e., regions with odd winding are considered part
+ * of the shape, whereas regions with even winding are not.
+ *
+ * For this reason, the two path-vectors are sometimes called "shapes" or "operands" of
+ * the boolean operation. Each path-vector may contain several paths, which are called
+ * either "paths" or "components" in the documentation.
+ *
+ * @ingroup Paths
+ */
 class PathIntersectionGraph
 {
     // this is called PathIntersectionGraph so that we can also have a class for polygons,
     // e.g. PolygonIntersectionGraph, which is going to be significantly faster
 public:
+    /** @brief Construct a path intersection graph for two shapes described via their boundaries.
+     *  The boundaries are passed as path-vectors.
+     *
+     *  @param a – the first operand, also referred to as operand A.
+     *  @param b – the second operand, also referred to as operand B.
+     *  @param precision – precision setting used for intersection calculations.
+     */
     PathIntersectionGraph(PathVector const &a, PathVector const &b, Coord precision = EPSILON);
 
+    /**
+     * @brief Get the union of the shapes, A ∪ B.
+     *
+     * A point belongs to the union if and only if it belongs to at least one of the operands.
+     *
+     * @return A path-vector describing the union of the operands A and B.
+     */
     PathVector getUnion();
+
+    /**
+     * @brief Get the intersection of the shapes, A ∩ B.
+     *
+     * A point belongs to the intersection if and only if it belongs to both shapes.
+     *
+     * @return A path-vector describing the intersection of the operands A and B.
+     */
     PathVector getIntersection();
+
+    /**
+     * @brief Get the difference of the shapes, A ∖ B.
+     *
+     * A point belongs to the difference if and only if it belongs to A but not to B.
+     *
+     * @return A path-vector describing the difference of the operands A and B.
+     */
     PathVector getAminusB();
+
+    /**
+     * @brief Get the opposite difference of the shapes, B ∖ A.
+     *
+     * A point belongs to the difference if and only if it belongs to B but not to A.
+     *
+     * @return A path-vector describing the difference of the operands B and A.
+     */
     PathVector getBminusA();
+
+    /**
+     * @brief Get the symmetric difference of the shapes, A ∆ B.
+     *
+     * A point belongs to the symmetric difference if and only if it belongs to one of the two
+     * shapes A or B, but not both. This is equivalent to the logical XOR operation: the elements
+     * of A ∆ B are points which are in A XOR in B.
+     *
+     * @return A path-vector describing the symmetric difference of the operands A and B.
+     */
     PathVector getXOR();
 
     /// Returns the number of intersections used when computing Boolean operations.
     std::size_t size() const;
+
+    /**
+     * @brief Get the geometric points where the two path-vectors intersect.
+     *
+     * Degenerate intersection points, where the shapes merely "kiss", are not retured.
+     *
+     * @param defective – whether to return only the defective crossings or only the true crossings.
+     * @return If defective is true, returns a vector containing all defective intersection points,
+     * i.e., points that are neither true transverse intersections nor degenerate intersections.
+     * If defective is false, returns all true transverse intersections.
+     */
     std::vector<Point> intersectionPoints(bool defective = false) const;
+
+    /**
+     * @brief Get the geometric points located on path portions between consecutive intersections.
+     *
+     * These points were used for the winding number calculations which determined which path portions
+     * lie inside the other shape and which lie outside.
+     *
+     * @return A vector containing all sample points used for winding calculations.
+     */
     std::vector<Point> windingPoints() const {
         return _winding_points;
     }
+
     void fragments(PathVector &in, PathVector &out) const;
+
+
     bool valid() const { return _graph_valid; }
 
 private:
@@ -75,11 +163,18 @@ private:
     struct IntersectionVertex {
         boost::intrusive::list_member_hook<> _hook;
         boost::intrusive::list_member_hook<> _proc_hook;
-        PathVectorTime pos;
-        Point p; // guarantees that endpoints are exact
-        IntersectionVertex *neighbor;
+        PathVectorTime pos; ///< Intersection time.
+        Point p; ///< Geometric position of the intersection point; guarantees that endpoints are exact.
+        IntersectionVertex *neighbor; ///< A pointer to the corresponding vertex on the other shape.
+        /** Tells us whether the edge originating at this intersection lies inside or outside of
+         *  the shape given by the other path-vector. The "edge originating" at this intersection is
+         *  the portion of the path between this intersection and the next intersection, in the
+         *  direction of increasing path time. */
         InOutFlag next_edge;
-        unsigned which;
+        unsigned which; ///< Index of the operand path-vector that this intersection vertex lies on.
+        /** Whether the intersection is defective, which means that for some reason the paths
+         *  neither cross transversally through each other nor "kiss" at a common tangency point.
+         */
         bool defective;
     };
 
@@ -101,10 +196,14 @@ private:
             >
         > UnprocessedList;
 
+    /// Stores processed intersection information for a single path in an operand path-vector.
     struct PathData {
-        IntersectionList xlist;
-        std::size_t path_index;
-        int which;
+        IntersectionList xlist; ///< List of crossings on this particular path.
+        std::size_t path_index; ///< Index of the path in its path-vector.
+        int which; ///< Index of the path-vector (in PathIntersectionGraph::_pv) that the path belongs to.
+        /** Whether this path as a whole is contained INSIDE or OUTSIDE relative to the other path-vector.
+         *  The value BOTH means that some portions of the path are inside while others are outside.
+         */
         InOutFlag status;
 
         PathData(int w, std::size_t pi)
@@ -130,11 +229,14 @@ private:
     ILIter _getNeighbor(ILIter iter);
     PathData &_getPathData(ILIter iter);
 
-    PathVector _pv[2];
-    boost::ptr_vector<IntersectionVertex> _xs;
-    boost::ptr_vector<PathData> _components[2];
-    UnprocessedList _ulist;
-    bool _graph_valid;
+    PathVector _pv[2]; ///< Stores the two operand path-vectors, A at _pv[0] and B at _pv[1].
+    boost::ptr_vector<IntersectionVertex> _xs; ///< Stores all crossings between the two shapes.
+    boost::ptr_vector<PathData> _components[2]; ///< Stores the crossing information for the operands.
+    UnprocessedList _ulist; ///< Temporarily holds all unprocessed during a boolean operation.
+    bool _graph_valid; ///< Whether all intersections are regular.
+    /** Stores sample points located on paths of the operand path-vectors,
+     *  between consecutive intersections.
+     */
     std::vector<Point> _winding_points;
 
     friend std::ostream &operator<<(std::ostream &, PathIntersectionGraph const &);
--- src/2geom/intersection-graph.cpp
+++ src/2geom/intersection-graph.cpp
@@ -40,21 +40,12 @@
 
 namespace Geom {
 
+/// Function object for comparing intersection vertices based on the intersection time.
 struct PathIntersectionGraph::IntersectionVertexLess {
     bool operator()(IntersectionVertex const &a, IntersectionVertex const &b) const {
         return a.pos < b.pos;
     }
 };
-
-/** @class PathIntersectionGraph
- * @brief Intermediate data for computing Boolean operations on paths.
- *
- * This class implements the Greiner-Hormann clipping algorithm,
- * with improvements inspired by Foster and Overfelt as well as some
- * original contributions.
- *
- * @ingroup Paths
- */
 
 PathIntersectionGraph::PathIntersectionGraph(PathVector const &a, PathVector const &b, Coord precision)
     : _graph_valid(true)
@@ -69,6 +60,10 @@ PathIntersectionGraph::PathIntersectionGraph(PathVector const &a, PathVector con
     if (!has_intersections) return;
 
     _assignEdgeWindingParities(precision);
+
+    // If a path has only degenerate intersections, assign its status now.
+    // This protects against later accidentally picking a point for winding
+    // determination that is exactly at a removed intersection.
     _assignComponentStatusFromDegenerateIntersections();
     _removeDegenerateIntersections();
     if (_graph_valid) {
@@ -76,6 +71,9 @@ PathIntersectionGraph::PathIntersectionGraph(PathVector const &a, PathVector con
     }
 }
 
+/** Prepare the operands stored in PathIntersectionGraph::_pv by closing all of their constituent
+ *  paths and removing degenerate segments from them.
+ */
 void PathIntersectionGraph::_prepareArguments()
 {
     // all paths must be closed, otherwise we will miss some intersections
@@ -100,6 +98,10 @@ void PathIntersectionGraph::_prepareArguments()
     }
 }
 
+/** @brief Compute the lists of intersections between the constituent paths of both operands.
+ *  @param precision – the precision setting for the sweepline algorithm.
+ *  @return Whether any intersections were found.
+ */
 bool PathIntersectionGraph::_prepareIntersectionLists(Coord precision)
 {
     std::vector<PVIntersection> pxs = _pv[0].intersect(_pv[1], precision);
@@ -134,7 +136,7 @@ bool PathIntersectionGraph::_prepareIntersectionLists(Coord precision)
         _components[1][xb->pos.path_index].xlist.push_back(*xb);
     }
 
-    // sort components according to time value of intersections
+    // sort intersections in each component according to time value
     for (auto & _component : _components) {
         for (std::size_t i = 0; i < _component.size(); ++i) {
             _component[i].xlist.sort(IntersectionVertexLess());
@@ -144,18 +146,21 @@ bool PathIntersectionGraph::_prepareIntersectionLists(Coord precision)
     return true;
 }
 
+/** Determine whether path portions between consecutive intersections lie inside or outside
+ *  of the other path-vector.
+ */
 void PathIntersectionGraph::_assignEdgeWindingParities(Coord precision)
 {
-    // determine the winding numbers of path portions between intersections
     for (unsigned w = 0; w < 2; ++w) {
-        unsigned ow = (w+1) % 2;
+        unsigned ow = (w+1) % 2; ///< The index of the other operand
 
-        for (unsigned li = 0; li < _components[w].size(); ++li) {
+        for (unsigned li = 0; li < _components[w].size(); ++li) { // Traverse all paths in the component
             IntersectionList &xl = _components[w][li].xlist;
-            for (ILIter i = xl.begin(); i != xl.end(); ++i) {
+            for (ILIter i = xl.begin(); i != xl.end(); ++i) { // Traverse all intersections in the path
                 ILIter n = cyclic_next(i, xl);
                 std::size_t pi = i->pos.path_index;
 
+                /// Path time interval from the current crossing to the next one
                 PathInterval ival = forward_interval(i->pos, n->pos, _pv[w][pi].size());
                 PathTime mid = ival.inside(precision);
 
@@ -172,11 +177,11 @@ void PathIntersectionGraph::_assignEdgeWindingParities(Coord precision)
     }
 }
 
+/** Detect the situation where a path is either entirely inside or entirely outside of the other
+ * path-vector and set the status flag accordingly.
+ */
 void PathIntersectionGraph::_assignComponentStatusFromDegenerateIntersections()
 {
-    // If a path has only degenerate intersections, assign its status now.
-    // This protects against later accidentally picking a point for winding
-    // determination that is exactly at a removed intersection.
     for (auto & _component : _components) {
         for (unsigned li = 0; li < _component.size(); ++li) {
             IntersectionList &xl = _component[li].xlist;
@@ -196,32 +201,36 @@ void PathIntersectionGraph::_assignComponentStatusFromDegenerateIntersections()
     }
 }
 
+/** Remove intersections that don't change between in/out.
+ *
+ * In general, a degenerate intersection can happen at a point where
+ * two shapes "kiss" (are tangent) but do not cross into each other.
+ */
 void PathIntersectionGraph::_removeDegenerateIntersections()
 {
-    // remove intersections that don't change between in/out
     for (auto & _component : _components) {
         for (unsigned li = 0; li < _component.size(); ++li) {
             IntersectionList &xl = _component[li].xlist;
             for (ILIter i = xl.begin(); i != xl.end();) {
                 ILIter n = cyclic_next(i, xl);
-                if (i->next_edge == n->next_edge) {
-                    bool last_node = (i == n);
+                if (i->next_edge == n->next_edge) { // Both edges inside or both outside
+                    bool last_node = (i == n); ///< Whether this is the last remaining crossing.
                     ILIter nn = _getNeighbor(n);
                     IntersectionList &oxl = _getPathData(nn).xlist;
 
                     // When exactly 3 out of 4 edges adjacent to an intersection
                     // have the same winding, we have a defective intersection,
                     // which is neither degenerate nor normal. Those can occur in paths
-                    // that contain overlapping segments. We cannot handle that case
-                    // for now, so throw an exception.
+                    // that contain overlapping segments.
                     if (cyclic_prior(nn, oxl)->next_edge != nn->next_edge) {
+                        // Not a backtrack - set the defective flag.
                         _graph_valid = false;
                         n->defective = true;
                         nn->defective = true;
                         ++i;
                         continue;
                     }
-
+                    // Erase the degenerate or defective crossings
                     oxl.erase(nn);
                     xl.erase(n);
                     if (last_node) break;
@@ -233,6 +242,9 @@ void PathIntersectionGraph::_removeDegenerateIntersections()
     }
 }
 
+/** Verify that all paths contain an even number of intersections and that
+ *  the intersection graph does not contain leaves (degree one vertices).
+ */
 void PathIntersectionGraph::_verify()
 {
 #ifndef NDEBUG
@@ -335,12 +347,25 @@ void PathIntersectionGraph::fragments(PathVector &in, PathVector &out) const
     }
 }
 
+/** @brief Compute the partial result of a boolean operation by looking at components containing
+ *  intersections and stitching the correct path portions between them, depending on the truth
+ *  table of the operation.
+ *
+ * @param enter_a – whether the path portions contained inside operand A should be part of the boundary
+ *                  of the boolean operation's result.
+ * @param enter_b – whether the path portions contained inside operand B should be part of the boundary
+ *                  of the boolean operation's result.
+ *
+ * These two flags completely determine how to resolve the crossings when building the result
+ * and therefore encode which boolean operation we are performing. For example, the boolean intersection
+ * corresponds to enter_a == true and enter_b == true, as can be seen by looking at a Venn diagram.
+ */
 PathVector PathIntersectionGraph::_getResult(bool enter_a, bool enter_b)
 {
     PathVector result;
     if (_xs.empty()) return result;
 
-    // reset processed status
+    // Create the list of intersections to process
     _ulist.clear();
     for (auto & _component : _components) {
         for (auto & li : _component) {
@@ -361,19 +386,18 @@ PathVector PathIntersectionGraph::_getResult(bool enter_a, bool enter_b)
 
         result.push_back(Path(i->p));
         result.back().setStitching(true);
-        bool reverse = false;
+        bool reverse = false; ///< Whether to traverse the current component in the backwards direction.
         while (i->_proc_hook.is_linked()) {
             ILIter prev = i;
-            std::size_t pi = i->pos.path_index;
+            std::size_t pi = i->pos.path_index; ///< Index of the path in its PathVector
             // determine which direction to go
             // union: always go outside
             // intersection: always go inside
             // a minus b: go inside in b, outside in a
             // b minus a: go inside in a, outside in b
-            reverse = false;
-            if (w == 0) {
+            if (w == 0) { // The path we're on is a part of A
                 reverse = (i->next_edge == INSIDE) ^ enter_a;
-            } else {
+            } else { // The path we're on is a part of B
                 reverse = (i->next_edge == INSIDE) ^ enter_b;
             }
 
@@ -384,16 +408,14 @@ PathVector PathIntersectionGraph::_getResult(bool enter_a, bool enter_b)
                 i = cyclic_next(i, _components[w][pi].xlist);
             }
 
-            // append portion of path
+            // append portion of path to the result
             PathInterval ival = PathInterval::from_direction(
                 prev->pos.asPathTime(), i->pos.asPathTime(),
                 reverse, _pv[i->which][pi].size());
 
             _pv[i->which][pi].appendPortionTo(result.back(), ival, prev->p, i->p);
 
-            // mark both vertices as processed
-            //prev->processed = true;
-            //i->processed = true;
+            // count both vertices as processed
             n_processed += 2;
             if (prev->_proc_hook.is_linked()) {
                 _ulist.erase(_ulist.iterator_to(*prev));
@@ -424,14 +446,20 @@ PathVector PathIntersectionGraph::_getResult(bool enter_a, bool enter_b)
     return result;
 }
 
+/** @brief Select intersection-free path components ahead of a boolean operation based on whether
+ *  they should be a part of that operation's result.
+ *
+ * Every component that has intersections will be processed by _getResult().
+ * Here we take care of paths that don't have any intersections. They are either
+ * completely inside or completely outside the other path-vector.
+ *
+ * @param result – output parameter to store the selected components.
+ * @param which – which of the two operands to search for intersection-free paths.
+ * @param inside – If set to true, add paths entirely contained inside the other path-vector to
+ *    the result. If set to false, add paths entirely outside of the other path-vector instead.
+ */
 void PathIntersectionGraph::_handleNonintersectingPaths(PathVector &result, unsigned which, bool inside)
 {
-    /* Every component that has any intersections will be processed by _getResult.
-     * Here we take care of paths that don't have any intersections. They are either
-     * completely inside or completely outside the other pathvector. We test this by
-     * evaluating the winding rule at the initial point. If inside is true and
-     * the path is inside, we add it to the result.
-     */
     unsigned w = which;
     unsigned ow = (w+1) % 2;
 
@@ -442,12 +470,13 @@ void PathIntersectionGraph::_handleNonintersectingPaths(PathVector &result, unsi
         if (has_path_data && !_components[w][i].xlist.empty()) continue;
         bool path_inside = false;
 
-        // Use the in/out determination from constructor, if available
+        // Use the status flag set in the constructor if available.
         if (has_path_data && _components[w][i].status == INSIDE) {
             path_inside = true;
         } else if (has_path_data && _components[w][i].status == OUTSIDE) {
             path_inside = false;
         } else {
+            // The status flag is ambiguous: we evaluate the winding number of the initial point.
             int wdg = _pv[ow].winding(_pv[w][i].initialPoint());
             path_inside = wdg % 2 != 0;
         }
@@ -458,18 +487,25 @@ void PathIntersectionGraph::_handleNonintersectingPaths(PathVector &result, unsi
     }
 }
 
+/** @brief Get an iterator to the corresponding crossing on the other path-vector.
+ *
+ *  @param ILIter – an iterator to a list of intersections in one of the path-vectors.
+ *  @return An iterator to the corresponding intersection in the other path-vector.
+ */
 PathIntersectionGraph::ILIter PathIntersectionGraph::_getNeighbor(ILIter iter)
 {
     unsigned ow = (iter->which + 1) % 2;
     return _components[ow][iter->neighbor->pos.path_index].xlist.iterator_to(*iter->neighbor);
 }
 
+/** Get the path data for the path containing the intersection given an iterator to the intersection */
 PathIntersectionGraph::PathData &
 PathIntersectionGraph::_getPathData(ILIter iter)
 {
     return _components[iter->which][iter->pos.path_index];
 }
 
+/** Format the PathIntersectionGraph for output. */
 std::ostream &operator<<(std::ostream &os, PathIntersectionGraph const &pig)
 {
     os << "Intersection graph:\n"
