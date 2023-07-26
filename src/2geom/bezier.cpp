0d48d5b79d4136a619ae8cfc2dc14114471b5df2
PBS <pbs3141@gmail.com>
2023-07-26 22:10:27 +0900

    Work around GCC null-analyser false positives
    Fixes https://gitlab.com/inkscape/lib2geom/-/issues/64

--- src/2geom/bezier.cpp
+++ src/2geom/bezier.cpp
@@ -40,6 +40,12 @@
 #include <2geom/concepts.h>
 #include <2geom/choose.h>
 
+// Workaround for GCC null-analyser false positives.
+// See https://gitlab.com/inkscape/lib2geom/-/issues/64
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC diagnostic ignored "-Wnonnull"
+#endif
+
 namespace Geom {
 
 std::vector<Coord> Bezier::valueAndDerivatives(Coord t, unsigned n_derivs) const {
